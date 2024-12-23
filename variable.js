36bda3618c6f39d971a7cc76d611026b6ee55e34
malisurajm <surajmali22127@gmail.com>
2024-12-23 13:42:19 +0530

    Updated file

--- variable.js
+++ variable.js
@@ -1,1 +1,8 @@
-console.log("Hello From Node Js");
+// var , let and const
+
+{
+    var a = 10;
+    console.log("Accessing a inside the  block: ", a);
+    
+}
+console.log("Accessing a ouside the block: ", a);
