12f1c63ab90eff19f5b232ce8bbc44f63752af3e
malisurajm <surajmali22127@gmail.com>
2024-12-24 10:09:52 +0530

    Data Types.

---
+++ DataTypes.js
@@ -0,0 +1,26 @@
+// Number Data Types: All Numbers (whole, positive, negative)
+
+var a = 79788983.9389;
+console.log(a, typeof a);
+
+// String: Single cahracer or series of characters all are Strings
+var b = 'KodNest';
+console.log(b, typeof b);
+
+//Boolean: true/ false
+var c = true;
+console.log(c, typeof c);
+
+//Undefined:
+let d;
+console.log(d, typeof d);
+
+// Null
+let e = null;
+console.log(e, typeof e);
+
+
+
+
+
+
